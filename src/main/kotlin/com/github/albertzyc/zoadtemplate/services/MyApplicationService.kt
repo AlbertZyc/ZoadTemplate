200dc7232d2008372934275b00daaac5adbb50c2
GitHub Action <action@github.com>
2021-09-27 02:50:05 +0000

    Template cleanup

--- src/main/kotlin/com/github/albertzyc/zoadtemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/albertzyc/zoadtemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.albertzyc.zoadtemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.albertzyc.zoadtemplate.MyBundle
 
 class MyApplicationService {
 
